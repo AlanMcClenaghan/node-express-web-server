22b1895a28ebe57783d9da72f45922e3f848ad8b
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-06-10 16:58:12 +0100

    Serving up Static Assets.

--- src/app.js
+++ src/app.js
@@ -1,18 +1,13 @@
+const path = require("path");
 const express = require("express");
 
+console.log(__dirname);
+console.log(path.join(__dirname, "../public/about"));
+
 const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
 
-app.get("", (req, res) => {
-  res.send("<h1>Weather App</h1>");
-});
-
-app.get("/help", (req, res) => {
-  res.send("<h1>Help</h1>");
-});
-
-app.get("/about", (req, res) => {
-  res.send("<h1>About</h1>");
-});
+app.use(express.static(publicDirectoryPath));
 
 app.get("/weather", (req, res) => {
   res.send({
