b2683f24bba75021d85facd554ba031512cfd597
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-06-10 19:20:36 +0100

    Dynamic pages with Templating.

--- src/app.js
+++ src/app.js
@@ -7,7 +7,30 @@ console.log(path.join(__dirname, "../public/about"));
 const app = express();
 const publicDirectoryPath = path.join(__dirname, "../public");
 
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Alan McClenaghan"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Alan McClenaghan"
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    name: "Alan McClenaghan",
+    body: "I get by with a little help from my friends."
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({
