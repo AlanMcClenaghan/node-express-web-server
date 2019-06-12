ded739f30812dee8ad3f80562bf76643da748e83
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-06-12 15:41:39 +0100

    Search Form and Userinterface.

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,26 @@
 console.log("Client side javascript file is loaded!");
 
-// fetch("http://puzzle.mead.io/puzzle").then(response => {
-//   response.json().then(data => {
-//     console.log(data);
-//   });
-// });
+const weatherForm = document.querySelector("form");
+const message = document.querySelector("#message");
 
-fetch("http://localhost:3000/weather?address=London").then(response => {
-  response.json().then(data => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
-    }
+weatherForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const location = weatherForm.children[0].value;
+
+  message.innerHTML = "<i>Loading...</i>";
+
+  fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        message.innerHTML = `<b class="error">${data.error}<b>`;
+      } else {
+        message.innerHTML = `
+          <b>${data.forecast}</b>
+          <br>
+          <i>${data.location}</i>
+        `;
+      }
+    });
   });
 });
